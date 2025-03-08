72a8c0249b5ff42f0f010d89d9b75f4b21827b74
Asadul Islam <123383609+Asadul1120@users.noreply.github.com>
2025-03-08 12:07:32 -0600

    Update employer.model.js 08-03-25

--- models/employer.model.js
+++ models/employer.model.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const dutySchema = new mongoose.Schema({
-  date: { type: String, required: true },
+  date: { type: String, required: true, unique: true ,trim: true},
   shift: { type: String, required: true, uppercase: true },
   OT: { type: Number, required: true, min: 0 },
 });
 
 const employeeSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Name is required"], trim: true },
-  ID: { type: String, required: true, unique: true }, // Number -> String করা হলো
+  ID: { type: String, required: true, unique: true }, 
   phone: { type: String },
   line: { type: String, required: true },
   group: { type: String, required: true },
